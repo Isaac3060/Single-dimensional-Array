8282598d6bb655c3b2cb745f9ff12ccb6c1f682a
Isaac3060 <50213336+Isaac3060@users.noreply.github.com>
2019-06-17 07:22:58 -0400

    Add files via upload

---
+++ BasicArray.java
@@ -0,0 +1,46 @@
+
+public class BasicArray {
+
+    
+    public static void main(String[] args) {
+  
+        double[]myList = {1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9};
+        printValues(myList);
+        sumValues(myList);
+        maxValue(myList);
+        
+    }
+    
+    private static void printValues(double[]myList){
+        
+        for(int i=0;i<myList.length;i++){
+            System.out.println(myList[i]);
+        }
+     
+    }
+    
+    private static void sumValues(double[]myList){
+        double sum=0;
+        for(int i=0; i<myList.length; i++){
+        
+            sum+=myList[i];
+     
+    }
+        System.out.println("The sum of the array is " +sum);
+    }
+    
+    private static void maxValue(double[]myList){
+        double max =0;
+        for(double value:myList){
+            if(value>max){
+                max=value;
+            }
+            
+            
+        }
+        
+        System.out.println("The maximum value is " +max);
+        
+    }
+            
+}
